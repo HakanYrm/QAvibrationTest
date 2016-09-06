72d70a61418cfdd8d68b1d0a413c519e6d971288
Hakan <hakanyarimoglu@gmail.com>
2016-09-06 16:45:41 +0300

    Gereksizler silindi. (Menü, ıvırzıvır)

--- app/build/generated/source/r/debug/com/prgguru/android/R.java
+++ app/build/generated/source/r/debug/com/prgguru/android/R.java
@@ -16,25 +16,19 @@ public final class R {
         public static final int vestel=0x7f020002;
     }
     public static final class id {
-        public static final int btn1=0x7f070003;
-        public static final int btn2=0x7f070004;
-        public static final int chronometer1=0x7f070005;
-        public static final int menu_settings=0x7f070006;
-        public static final int textView=0x7f070000;
-        public static final int textView2=0x7f070002;
-        public static final int zaman=0x7f070001;
+        public static final int btn1=0x7f060003;
+        public static final int btn2=0x7f060004;
+        public static final int chronometer1=0x7f060005;
+        public static final int textView=0x7f060000;
+        public static final int textView2=0x7f060002;
+        public static final int zaman=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
-    public static final class menu {
-        public static final int main=0x7f060000;
-    }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_vibrator_example=0x7f050003;
+        public static final int title_activity=0x7f050001;
     }
     public static final class style {
         public static final int AppTheme=0x7f040000;
